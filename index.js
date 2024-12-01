fcd9346715890b4f137247b0580bfeb7c1a2bcce
AliHasan06 <alihassanraja06@gmail.com>
2024-12-01 20:53:44 +0500

    more

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.set("view", "ejs");
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("chal rha hai");
+  res.render("index");
 });
 
 app.listen(3000);
